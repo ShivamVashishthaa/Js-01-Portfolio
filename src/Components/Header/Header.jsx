9a789922edcff73b2e0ce4bc7566cd0cbfd8f8fb
Shivam Vashishtha <shivam283097@gmail.com>
2024-09-04 19:58:07 +0530

    add-downloadCV-link

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <header className='flex justify-between items-center bg-gradient-to-b from-orange-400 to-white dark:bg-slate-700  px-4 lg:px-11 py-4'>
+    <header className='flex justify-between items-center bg-gradient-to-b from-orange-400 to-white  px-4 lg:px-11 py-4'>
       <div className='lg:hidden'>
         <svg className='w-4 h-auto md:w-6' xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" /></svg>
       </div>
